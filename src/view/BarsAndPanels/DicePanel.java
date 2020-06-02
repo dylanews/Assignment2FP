3cc757f339fa797f46078e779bb173c9c94947af
Dylan <s3699661@student.rmit.edu.au>
2020-06-03 01:09:23 +0800

    update with dice panels

--- src/view/BarsAndPanels/DicePanel.java
+++ src/view/BarsAndPanels/DicePanel.java
@@ -1,26 +1,71 @@
 package view.BarsAndPanels;
 
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
 import model.interfaces.GameEngine;
+import model.interfaces.Player;
 import view.GameEngineWindow;
 
-public class DicePanel {
-
+public class DicePanel extends JPanel {
+	
+		private GameEngine gameEngine;
+		private GameEngineWindow gameEngineWindow;
+		private Player activePlayer;
+		
+		private static final int SIDE = 32;
+		
 	public DicePanel(GameEngine gameEngine, GameEngineWindow gameEngineWindow) {
-		// TODO Auto-generated constructor stub
+
+		this.gameEngine = gameEngine;
+		this.gameEngineWindow = gameEngineWindow;
+		
 	}
 	
-	//paint image
-	// - need to resize image depending on window size
+	 @Override
+	  public void paintComponent(Graphics g) {
+	    super.paintComponent(g);
+	    g.setColor(color);
+	    switch (value) {
+	    case 1:
+	      g.fillRect(3 * SIDE, 3 * SIDE, SIDE, SIDE);
+	      break;
+	    case 2:
+	      g.fillRect(5 * SIDE, SIDE, SIDE, SIDE);
+	      g.fillRect(SIDE, 5 * SIDE, SIDE, SIDE);
+	      break;
+	    case 3:
+	      g.fillRect(5 * SIDE, SIDE, SIDE, SIDE);
+	      g.fillRect(SIDE, 5 * SIDE, SIDE, SIDE);
+	      g.fillRect(3 * SIDE, 3 * SIDE, SIDE, SIDE);
+	      break;
+	    case 4:
+	      g.fillRect(SIDE, SIDE, SIDE, SIDE);
+	      g.fillRect(5 * SIDE, 5 * SIDE, SIDE, SIDE);
+	      g.fillRect(5 * SIDE, SIDE, SIDE, SIDE);
+	      g.fillRect(SIDE, 5 * SIDE, SIDE, SIDE);
+	      break;
+	    case 5:
+	      g.fillRect(SIDE, SIDE, SIDE, SIDE);
+	      g.fillRect(5 * SIDE, 5 * SIDE, SIDE, SIDE);
+	      g.fillRect(5 * SIDE, SIDE, SIDE, SIDE);
+	      g.fillRect(SIDE, 5 * SIDE, SIDE, SIDE);
+	      g.fillRect(3 * SIDE, 3 * SIDE, SIDE, SIDE);
+	      break;
+	    case 6:
+	      g.fillRect(SIDE, SIDE, SIDE, SIDE);
+	      g.fillRect(5 * SIDE, 5 * SIDE, SIDE, SIDE);
+	      g.fillRect(5 * SIDE, SIDE, SIDE, SIDE);
+	      g.fillRect(SIDE, 5 * SIDE, SIDE, SIDE);
+	      g.fillRect(SIDE, 3 * SIDE, SIDE, SIDE);
+	      g.fillRect(5 * SIDE, 3 * SIDE, SIDE, SIDE);
+	      break;
+	    }
+	  }
 	
-	// update image
-	// -  update dice depending on active player
 	
-	// get image
-	// - return image
 	
-	// Change active player
-	// -  get dice depending on active player
 	
-	// update active player dice
-	// - update image
-}
+	
+	}
